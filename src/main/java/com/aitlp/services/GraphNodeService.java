0c796d1720c2252179be09536c2ab59c65bfb48b
aitlp <aitlpmw@gmail.com>
2019-09-20 11:13:01 +0800

    优化保存方法；

--- src/main/java/com/aitlp/services/GraphNodeService.java
+++ src/main/java/com/aitlp/services/GraphNodeService.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
@@ -22,6 +23,7 @@ public class GraphNodeService {
         return graphNodeRepository.findByName(name);
     }
 
+    @Transactional
     public Map save(GraphNode graphNode) {
         Map resultMap = new HashMap();
         String code = "200";
