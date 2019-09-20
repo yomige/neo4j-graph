00f98d99c2b97c3125b6db25eb7716f1f9ea21fe
aitlp <aitlpmw@gmail.com>
2019-09-20 10:44:02 +0800

    自定义节点ID；

---
+++ src/main/java/com/aitlp/util/Neo4JCustomIdStrategy.java
@@ -0,0 +1,14 @@
+package com.aitlp.util;
+
+import org.neo4j.ogm.id.IdStrategy;
+
+/**
+ * Define entity custom id strategy
+ */
+public class Neo4JCustomIdStrategy implements IdStrategy {
+    @Override
+    public Object generateId(Object o) {
+        return o.hashCode();
+    }
+
+}
